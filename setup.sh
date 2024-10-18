9fc5d29ffda5166df16b06408ebecb2056f89140
ch <mkmsacc@outlook.com>
2024-10-18 10:08:48 +0700

    Add setup script for WebStorm configuration
    This setup script automates the installation of keymaps, settings, and plugins for WebStorm. It downloads configuration files from a remote repository and installs them into the appropriate directories. Additionally, it installs the .ideavimrc configuration for Vim users.

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+# configs
+for config_path in \
+"$HOME"/Library/Application\ Support/JetBrains/WebStorm* \
+"$HOME"/AppData/Roaming/JetBrains/WebStorm* \
+
+do
+  echo "$config_path"
+  if [ -d "$config_path" ]; then
+
+    # install keymaps
+    mkdir -p "$config_path/keymaps"
+    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/keymaps/chubbyhippo.xml -o "$config_path/keymaps/chubbyhippo.xml"
+
+    # install settings
+    mkdir -p "$config_path/options"
+    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/editor.xml -o "$config_path/options/editor.xml"
+    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/projectView.xml -o "$config_path/options/projectView.xml"
+    mkdir -p "$config_path/options/mac"
+    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/mac/keymap.xml -o "$config_path/options/mac/keymap.xml"
+    mkdir -p "$config_path/options/windows"
+    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/windows/keymap.xml -o "$config_path/options/windows/keymap.xml"
+
+  fi
+done
+
+# install .ideavimrc
+curl -k https://raw.githubusercontent.com/chubbyhippo/ideavimrc/refs/heads/main/.ideavimrc -o ~/.ideavimrc
+
+# install plugins
+curl -k https://raw.githubusercontent.com/chubbyhippo/webstorm-settings/refs/heads/main/install-plugins.sh | /usr/bin/env sh
