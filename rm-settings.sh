f9815cdb4e2202874da9e5c0974d520cc489543d
ch <mkmsacc@outlook.com>
2024-10-18 10:08:37 +0700

    Add script to remove WebStorm configuration directories
    Create a new shell script, rm-settings.sh, that searches for and removes WebStorm configuration directories located in the user's home directory. This script checks directories in both macOS and Windows environments and deletes them if present.

---
+++ rm-settings.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+for config_path in \
+"$HOME"/Library/Application\ Support/JetBrains/WebStorm* \
+"$HOME"/AppData/Roaming/JetBrains/WebStorm* \
+
+do
+  echo "$config_path"
+  if [ -d "$config_path" ]; then
+    rm -rf "$config_path"
+  fi
+done
