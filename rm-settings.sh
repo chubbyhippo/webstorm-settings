0176af93653f18fe3ef3507d1b61793f6adc23a7
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-12-04 20:22:16 +0700

    Update rm-settings.sh

--- rm-settings.sh
+++ rm-settings.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
 for config_path in \
+"$HOME"/.config/JetBrains/WebStorm* \
 "$HOME"/Library/Application\ Support/JetBrains/WebStorm* \
-"$HOME"/AppData/Roaming/JetBrains/WebStorm* \
+"$HOME"/AppData/Roaming/JetBrains/WebStorm*
 
 do
   echo "$config_path"
