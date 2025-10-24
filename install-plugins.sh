ff6904b88f2639f62aec729b9ba27658953c6af5
chubbyhippo <mkmsacc@outlook.com>
2025-10-24 22:13:14 +0700

    Fix inconsistent indentation in install-plugins.sh script

--- install-plugins.sh
+++ install-plugins.sh
@@ -3,12 +3,12 @@
 # plugins
 cmd=webstorm
 if uname | grep -q "^MINGW"; then
-  cmd="$HOME"/AppData/Local/JetBrains/Toolbox/scripts/webstorm.cmd
+	cmd="$HOME"/AppData/Local/JetBrains/Toolbox/scripts/webstorm.cmd
 fi
 
 $cmd installPlugins \
-IdeaVIM \
-com.jetbrains.kmm \
-com.joshestein.ideavim-quickscope \
-eu.theblob42.idea.whichkey \
-com.haulmont.rcb
+	IdeaVIM \
+	com.jetbrains.kmm \
+	com.joshestein.ideavim-quickscope \
+	eu.theblob42.idea.whichkey \
+	com.haulmont.rcb
