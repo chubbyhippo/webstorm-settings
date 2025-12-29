0cc965acce717d833b383a37c7a4cb3391800407
chubbyhippo <mkmsacc@outlook.com>
2025-12-29 17:11:08 +0700

    Fix inconsistent indentation in install-plugins.sh script

--- install-plugins.sh
+++ install-plugins.sh
@@ -3,20 +3,20 @@
 # plugins
 cmd=webstorm
 if uname | grep -q "^MINGW"; then
-	cmd="$HOME"/AppData/Local/JetBrains/Toolbox/scripts/webstorm.cmd
+  cmd="$HOME"/AppData/Local/JetBrains/Toolbox/scripts/webstorm.cmd
 fi
 
 $cmd installPlugins \
-	IdeaVIM \
-	StringToolsPlugin \
-	com.fwdekker.randomness \
-	com.github.camork.fileExpander \
-	com.joshestein.ideavim-quickscope \
-	com.julienphalip.ideavim.peekaboo \
-	com.jetbrains.kmm \
-	com.joshestein.ideavim-quickscope \
-	eu.theblob42.idea.whichkey \
-	indent-rainbow.indent-rainbow \
-	izhangzhihao.rainbow.brackets.lite \
-    org.asciidoctor.intellij.asciidoc \
-	org.sonarlint.idea
+  IdeaVIM \
+  StringToolsPlugin \
+  com.fwdekker.randomness \
+  com.github.camork.fileExpander \
+  com.joshestein.ideavim-quickscope \
+  com.julienphalip.ideavim.peekaboo \
+  com.jetbrains.kmm \
+  com.joshestein.ideavim-quickscope \
+  eu.theblob42.idea.whichkey \
+  indent-rainbow.indent-rainbow \
+  izhangzhihao.rainbow.brackets.lite \
+  org.asciidoctor.intellij.asciidoc \
+  org.sonarlint.idea
