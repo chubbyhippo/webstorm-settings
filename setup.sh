b3975e648f068c9b0f8d1ae9471bb2ecf39a1dd8
chubbyhippo <mkmsacc@outlook.com>
2026-01-01 00:25:17 +0700

    Refactor setup.sh to use centralized variables for raw URLs

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+RAW_ITELLIJ_SETTINGS_URL="https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main"
+RAW_IDEAVIM_URL="https://raw.githubusercontent.com/chubbyhippo/ideavimrc/refs/heads/main"
+RAW_WEBSTORM_SETTINGS_URL="https://raw.githubusercontent.com/chubbyhippo/webstorm-settings/refs/heads/main"
+
 # config paths
 for config_path in \
   "$HOME"/.config/JetBrains/WebStorm* \
@@ -7,29 +11,32 @@ for config_path in \
   "$HOME"/AppData/Roaming/JetBrains/WebStorm*; do
   echo "$config_path"
   if [ -d "$config_path" ]; then
+    echo "$config_path"
+    # install code styles
+    mkdir -p "$config_path/codestyles"
+    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/codestyles/Default.xml" -o "$config_path/codestyles/Default.xml"
 
     # install keymaps
     mkdir -p "$config_path/keymaps"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/keymaps/chubbyhippo.xml -o "$config_path/keymaps/chubbyhippo.xml"
+    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/keymaps/chubbyhippo.xml" -o "$config_path/keymaps/chubbyhippo.xml"
 
     # install options
     mkdir -p "$config_path/options"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/editor.xml -o "$config_path/options/editor.xml"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/laf.xml -o "$config_path/options/laf.xml"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/colors.scheme.xml -o "$config_path/options/colors.scheme.xml"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/postfixTemplates.xml -o "$config_path/options/postfixTemplates.xml"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/projectView.xml -o "$config_path/options/projectView.xml"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/ui.lnf.xml -o "$config_path/options/ui.lnf.xml"
+    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/options/editor.xml" -o "$config_path/options/editor.xml"
+    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/options/laf.xml" -o "$config_path/options/laf.xml"
+    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/options/colors.scheme.xml" -o "$config_path/options/colors.scheme.xml"
+    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/options/postfixTemplates.xml" -o "$config_path/options/postfixTemplates.xml"
+    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/options/projectView.xml" -o "$config_path/options/projectView.xml"
+    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/options/ui.lnf.xml" -o "$config_path/options/ui.lnf.xml"
     mkdir -p "$config_path/options/mac"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/mac/keymap.xml -o "$config_path/options/mac/keymap.xml"
+    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/options/mac/keymap.xml" -o "$config_path/options/mac/keymap.xml"
     mkdir -p "$config_path/options/windows"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/windows/keymap.xml -o "$config_path/options/windows/keymap.xml"
-
+    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/options/windows/keymap.xml" -o "$config_path/options/windows/keymap.xml"
   fi
 done
 
 # install .ideavimrc
-curl -k https://raw.githubusercontent.com/chubbyhippo/ideavimrc/refs/heads/main/.ideavimrc -o ~/.ideavimrc
+curl -k "$RAW_IDEAVIM_URL/.ideavimrc" -o ~/.ideavimrc
 
 # install plugins
-curl -k https://raw.githubusercontent.com/chubbyhippo/webstorm-settings/refs/heads/main/install-plugins.sh | /usr/bin/env sh
+curl -k "$RAW_WEBSTORM_SETTINGS_URL/install-plugins.sh" | /usr/bin/env sh
