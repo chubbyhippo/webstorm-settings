179471d43b302890ad46421ad3075ae2209a523b
chubbyhippo <mkmsacc@outlook.com>
2026-01-05 19:20:43 +0700

    Use silent and show-error flags (`-ksS`) in all `curl` commands in `setup.sh`.

--- setup.sh
+++ setup.sh
@@ -14,29 +14,29 @@ for config_path in \
     echo "$config_path"
     # install code styles
     mkdir -p "$config_path/codestyles"
-    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/codestyles/Default.xml" -o "$config_path/codestyles/Default.xml"
+    curl -ksS "$RAW_ITELLIJ_SETTINGS_URL/config/codestyles/Default.xml" -o "$config_path/codestyles/Default.xml"
 
     # install keymaps
     mkdir -p "$config_path/keymaps"
-    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/keymaps/chubbyhippo.xml" -o "$config_path/keymaps/chubbyhippo.xml"
+    curl -ksS "$RAW_ITELLIJ_SETTINGS_URL/config/keymaps/chubbyhippo.xml" -o "$config_path/keymaps/chubbyhippo.xml"
 
     # install options
     mkdir -p "$config_path/options"
-    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/options/editor.xml" -o "$config_path/options/editor.xml"
-    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/options/laf.xml" -o "$config_path/options/laf.xml"
-    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/options/colors.scheme.xml" -o "$config_path/options/colors.scheme.xml"
-    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/options/postfixTemplates.xml" -o "$config_path/options/postfixTemplates.xml"
-    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/options/projectView.xml" -o "$config_path/options/projectView.xml"
-    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/options/ui.lnf.xml" -o "$config_path/options/ui.lnf.xml"
+    curl -ksS "$RAW_ITELLIJ_SETTINGS_URL/config/options/editor.xml" -o "$config_path/options/editor.xml"
+    curl -ksS "$RAW_ITELLIJ_SETTINGS_URL/config/options/laf.xml" -o "$config_path/options/laf.xml"
+    curl -ksS "$RAW_ITELLIJ_SETTINGS_URL/config/options/colors.scheme.xml" -o "$config_path/options/colors.scheme.xml"
+    curl -ksS "$RAW_ITELLIJ_SETTINGS_URL/config/options/postfixTemplates.xml" -o "$config_path/options/postfixTemplates.xml"
+    curl -ksS "$RAW_ITELLIJ_SETTINGS_URL/config/options/projectView.xml" -o "$config_path/options/projectView.xml"
+    curl -ksS "$RAW_ITELLIJ_SETTINGS_URL/config/options/ui.lnf.xml" -o "$config_path/options/ui.lnf.xml"
     mkdir -p "$config_path/options/mac"
-    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/options/mac/keymap.xml" -o "$config_path/options/mac/keymap.xml"
+    curl -ksS "$RAW_ITELLIJ_SETTINGS_URL/config/options/mac/keymap.xml" -o "$config_path/options/mac/keymap.xml"
     mkdir -p "$config_path/options/windows"
-    curl -k "$RAW_ITELLIJ_SETTINGS_URL/config/options/windows/keymap.xml" -o "$config_path/options/windows/keymap.xml"
+    curl -ksS "$RAW_ITELLIJ_SETTINGS_URL/config/options/windows/keymap.xml" -o "$config_path/options/windows/keymap.xml"
   fi
 done
 
 # install .ideavimrc
-curl -k "$RAW_IDEAVIM_URL/.ideavimrc" -o ~/.ideavimrc
+curl -ksS "$RAW_IDEAVIM_URL/.ideavimrc" -o ~/.ideavimrc
 
 # install plugins
-curl -k "$RAW_WEBSTORM_SETTINGS_URL/install-plugins.sh" | /usr/bin/env sh
+curl -ksS "$RAW_WEBSTORM_SETTINGS_URL/install-plugins.sh" | /usr/bin/env sh
