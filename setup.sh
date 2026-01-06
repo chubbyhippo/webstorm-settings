45654ded285d20241abeef3f722f5bb1468d177b
chubbyhippo <mkmsacc@outlook.com>
2026-01-06 21:16:07 +0700

    Remove unnecessary echo statement in setup.sh script

--- setup.sh
+++ setup.sh
@@ -9,7 +9,6 @@ for config_path in \
   "$HOME"/.config/JetBrains/WebStorm* \
   "$HOME"/Library/Application\ Support/JetBrains/WebStorm* \
   "$HOME"/AppData/Roaming/JetBrains/WebStorm*; do
-  echo "$config_path"
   if [ -d "$config_path" ]; then
     echo "$config_path"
     # install code styles
